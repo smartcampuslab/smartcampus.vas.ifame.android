1ecf46944d8be1ee11b9199c7c48dadedd51911d
desmond.agyeman <desmond.agyeman@3790a6ea-ec04-4a19-b1cd-c9a0372488fe>
2013-04-17 10:07:28 +0000

    iFretta : added home and details

---
+++ src/eu/trentorise/smartcampus/template/IFretta.java
@@ -0,0 +1,79 @@
+package eu.trentorise.smartcampus.template;
+
+import smartcampus.android.template.standalone.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class IFretta extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ifretta);
+
+		findViewById(R.id.povo_0).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+
+		findViewById(R.id.povo_1).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		findViewById(R.id.tommaso_gar).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		findViewById(R.id.mesiano).setOnClickListener(new OnClickListener()  {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		findViewById(R.id.zanella).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+
+		findViewById(R.id.fbk).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		findViewById(R.id.xxiv_magggio).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+	}
+}
