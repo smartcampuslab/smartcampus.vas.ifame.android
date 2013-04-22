73b181d33a8dd4f31a3931f9a028c697cf07ae4c
desmond.agyeman <desmond.agyeman@3790a6ea-ec04-4a19-b1cd-c9a0372488fe>
2013-04-22 08:31:40 +0000

    isoldi functionality and layout has been added..

--- src/eu/trentorise/smartcampus/ifame/activity/IFame_Main_Activity.java
+++ src/eu/trentorise/smartcampus/ifame/activity/IFame_Main_Activity.java
@@ -1,14 +1,15 @@
 package eu.trentorise.smartcampus.ifame.activity;
 
-import eu.trentorise.smartcampus.ifame.R;
-import eu.trentorise.smartcampus.template.MainActivity;
-import android.os.Bundle;
+import java.util.Random;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import eu.trentorise.smartcampus.ifame.R;
 
 public class IFame_Main_Activity extends Activity {
 
@@ -16,55 +17,62 @@ public class IFame_Main_Activity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_ifame_main);
-		
-		Button iFretta_btn = (Button)findViewById(R.id.iFretta_button);
+
+		Button iFretta_btn = (Button) findViewById(R.id.iFretta_button);
 		iFretta_btn.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-			
-			Intent i = new Intent (IFame_Main_Activity.this, IFretta.class);
-			startActivity(i);
-				
+
+				Intent i = new Intent(IFame_Main_Activity.this, IFretta.class);
+				startActivity(i);
+
 			}
 
 		});
-		
-		Button iDeciso_btn = (Button)findViewById(R.id.iDeciso_button);
+
+		Button iDeciso_btn = (Button) findViewById(R.id.iDeciso_button);
 		iDeciso_btn.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-			
-			Intent i = new Intent (IFame_Main_Activity.this, IDeciso.class);
-			startActivity(i);
-				
+
+				Intent i = new Intent(IFame_Main_Activity.this, IDeciso.class);
+				startActivity(i);
+
 			}
 
 		});
-		
-		Button iGradito_btn = (Button)findViewById(R.id.iGradito_button);
+
+		Button iGradito_btn = (Button) findViewById(R.id.iGradito_button);
 		iGradito_btn.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-			
-			Intent i = new Intent (IFame_Main_Activity.this, IGradito.class);
-			startActivity(i);
-				
+
+				Intent i = new Intent(IFame_Main_Activity.this, IGradito.class);
+				startActivity(i);
+
 			}
 
 		});
-		
-		Button iSoldi_btn = (Button)findViewById(R.id.iSoldi_button);
+
+		Button iSoldi_btn = (Button) findViewById(R.id.iSoldi_button);
 		iSoldi_btn.setOnClickListener(new OnClickListener() {
+
+			float card_val;
 
 			@Override
 			public void onClick(View v) {
-			
-			Intent i = new Intent (IFame_Main_Activity.this, ISoldi.class);
-			startActivity(i);
-				
+				float[] values = new float[] { 4.10f, 2.7f, 1.8f, 13.0f, 5.10f,
+						7.15f, 3.77f, 3.50f, 2.60f, 2.90f };
+				for (int i = 0; i < values.length; i++) {
+					card_val = values[new Random().nextInt(values.length)];
+				}
+				Intent i = new Intent(IFame_Main_Activity.this, ISoldi.class);
+				i.putExtra("iSoldi", card_val);
+				startActivity(i);
+
 			}
 
 		});
@@ -76,5 +84,5 @@ public class IFame_Main_Activity extends Activity {
 		getMenuInflater().inflate(R.menu.ifame__main_, menu);
 		return true;
 	}
-	
+
 }
--- src/eu/trentorise/smartcampus/ifame/activity/ISoldi.java
+++ src/eu/trentorise/smartcampus/ifame/activity/ISoldi.java
@@ -1,23 +1,55 @@
 package eu.trentorise.smartcampus.ifame.activity;
 
-import eu.trentorise.smartcampus.ifame.R;
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+import eu.trentorise.smartcampus.ifame.R;
 
 public class ISoldi extends Activity {
+	
+	TextView centerText; 
+	TextView bottomText; 
 
-	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_isoldi);
+
+		Bundle extras = getIntent().getExtras();
+		if (extras == null) {
+			return;
+		}
+
+		float card_value = (Float) extras.get("iSoldi");
+
+		centerText = (TextView) findViewById(R.id.isoldi_center_text);
+		bottomText = (TextView) findViewById(R.id.isoldi_bottom_text);
+
+		getAmount(card_value);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.isoldi, menu);
-		return true;
-	}
+	public void getAmount(float amount) {
+		if (amount >= 4.90) {
+			centerText.setText("€ " + String.valueOf(amount));
+			centerText.setTextColor(Color.parseColor("#228B22"));
 
+			bottomText.setText("Puoi acquistare tutte le tipologie di menu!");
+			bottomText.setTextColor(Color.parseColor("#228B22"));
+		} else if (amount >= 2.90 && amount < 4.90) {
+			centerText.setText("€ " + String.valueOf(amount));
+			centerText.setTextColor(Color.parseColor("#D2691E"));
+
+			bottomText
+					.setText("Non puoi acquistare tutte le tipologie di menu!");
+			bottomText.setTextColor(Color.parseColor("#D2691E"));
+		} else {
+			centerText.setText("€ " + String.valueOf(amount));
+			centerText.setTextColor(Color.parseColor("#FF0000"));
+
+			bottomText
+					.setText("Non puoi acquistare nessuna tipologia di menu!");
+			bottomText.setTextColor(Color.parseColor("#FF0000"));
+		}
+	}
 }
