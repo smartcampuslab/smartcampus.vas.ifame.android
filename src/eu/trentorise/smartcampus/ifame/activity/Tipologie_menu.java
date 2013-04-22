74f233daac127d2f849a8bd8f58450afdc5718b4
francesco.bonadiman <francesco.bonadiman@3790a6ea-ec04-4a19-b1cd-c9a0372488fe>
2013-04-22 14:56:51 +0000

    Tab menu for Tipologie di Menu added

--- src/eu/trentorise/smartcampus/ifame/activity/Tipologie_menu.java
+++ src/eu/trentorise/smartcampus/ifame/activity/Tipologie_menu.java
@@ -10,48 +10,66 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class Tipologie_menu extends Activity {
+public class Tipologie_menu extends TabActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_tipologie_menu);
 		
-		Button iDeciso_menu_intero_btn = (Button)findViewById(R.id.menu_intero_btn);
-		iDeciso_menu_intero_btn.setOnClickListener(new OnClickListener(){
-
-			@Override
-			public void onClick(View arg0) {
-				
-				Intent i = new Intent(Tipologie_menu.this, Menu_Intero.class);
-				startActivity(i);
-				
-			}});
+		TabHost menu = (TabHost)findViewById(android.R.id.tabhost);
+        TabSpec spec = menu.newTabSpec("menu_intero_btn");
+        spec.setContent(R.id.menu_intero_btn);
+        spec.setIndicator("Intero");
+        menu.addTab(spec);
+        spec = menu.newTabSpec("menu_ridotto_btn");
+        spec.setContent(R.id.menu_ridotto_btn);
+        spec.setIndicator("Ridotto");
+        menu.addTab(spec);
+        spec = menu.newTabSpec("menu_snack_btn");
+        spec.setContent(R.id.menu_snack_btn);
+        spec.setIndicator("Snack");
+        menu.addTab(spec);
 		
-		Button iDeciso_menu_ridotto_btn = (Button)findViewById(R.id.menu_ridotto_btn);
-		iDeciso_menu_ridotto_btn.setOnClickListener(new OnClickListener(){
-
-			@Override
-			public void onClick(View arg0) {
-				
-				Intent i = new Intent(Tipologie_menu.this, Menu_Ridotto.class);
-				startActivity(i);
-				
-			}});
-		
-		Button iDeciso_menu_snack_btn = (Button)findViewById(R.id.menu_snack_btn);
-		iDeciso_menu_snack_btn.setOnClickListener(new OnClickListener(){
-
-			@Override
-			public void onClick(View arg0) {
-				
-				Intent i = new Intent(Tipologie_menu.this, Menu_snack.class);
-				startActivity(i);
-				
-			}});	
-		
-		
+//		Button iDeciso_menu_intero_btn = (Button)findViewById(R.id.menu_intero_btn);
+//		iDeciso_menu_intero_btn.setOnClickListener(new OnClickListener(){
+//
+//			@Override
+//			public void onClick(View arg0) {
+//				
+//				Intent i = new Intent(Tipologie_menu.this, Menu_Intero.class);
+//				startActivity(i);
+//				
+//			}});
+//		
+//		Button iDeciso_menu_ridotto_btn = (Button)findViewById(R.id.menu_ridotto_btn);
+//		iDeciso_menu_ridotto_btn.setOnClickListener(new OnClickListener(){
+//
+//			@Override
+//			public void onClick(View arg0) {
+//				
+//				Intent i = new Intent(Tipologie_menu.this, Menu_Ridotto.class);
+//				startActivity(i);
+//				
+//			}});
+//		
+//		Button iDeciso_menu_snack_btn = (Button)findViewById(R.id.menu_snack_btn);
+//		iDeciso_menu_snack_btn.setOnClickListener(new OnClickListener(){
+//
+//			@Override
+//			public void onClick(View arg0) {
+//				
+//				Intent i = new Intent(Tipologie_menu.this, Menu_snack.class);
+//				startActivity(i);
+//				
+//			}});	
+//		
+//		
 	}
 
 	@Override
@@ -59,6 +77,7 @@ public class Tipologie_menu extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.tipologie_menu, menu);
 		return true;
+		
 	}
 
 }
