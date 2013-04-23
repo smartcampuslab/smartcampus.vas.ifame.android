34a157856294623d6368c0a62e80d4aef77799af
francesco.bonadiman <francesco.bonadiman@3790a6ea-ec04-4a19-b1cd-c9a0372488fe>
2013-04-23 13:53:18 +0000

    First functionality of Menu del Giorno added!

--- src/eu/trentorise/smartcampus/ifame/activity/Menu_giorno.java
+++ src/eu/trentorise/smartcampus/ifame/activity/Menu_giorno.java
@@ -1,20 +1,26 @@
 package eu.trentorise.smartcampus.ifame.activity;
 
 import eu.trentorise.smartcampus.ifame.R;
-import eu.trentorise.smartcampus.ifame.R.layout;
-import eu.trentorise.smartcampus.ifame.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import android.widget.TextView;
 
-public class Menu_giorno extends Activity {
+	public class Menu_giorno extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.layout_menu_giorno);
-	}
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.layout_menu_giorno);
 
+			final TextView date = (TextView) findViewById(R.id.date_daily_menu);
+
+			SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy");
+			String daily_menu = s.format(new Date());
+			date.setText(daily_menu); 
+			
+		}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
