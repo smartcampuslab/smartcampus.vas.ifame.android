9fce4456da538e11e53e0bbf898497719b72b763
francesco.bonadiman <francesco.bonadiman@3790a6ea-ec04-4a19-b1cd-c9a0372488fe>
2013-04-24 11:08:05 +0000

    iDeciso Menu del Giorno is ready! ;)

--- src/eu/trentorise/smartcampus/ifame/activity/Menu_giorno.java
+++ src/eu/trentorise/smartcampus/ifame/activity/Menu_giorno.java
@@ -1,11 +1,15 @@
 package eu.trentorise.smartcampus.ifame.activity;
 
 import eu.trentorise.smartcampus.ifame.R;
+import eu.trentorise.smartcampus.ifame.R.layout;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+
+import android.widget.ListView;
 import android.widget.TextView;
 
 	public class Menu_giorno extends Activity {
@@ -19,6 +23,50 @@ import android.widget.TextView;
 			SimpleDateFormat s = new SimpleDateFormat("EEEE dd MMMM yyyy");
 			String daily_menu = s.format(new Date());
 			date.setText(daily_menu); 
+			
+			//riempio la ListView per i primi piatti
+			
+			String[] primi = { "Pasta Aglio olio e peperoncino", "Pasta al ragu",
+					"Risotto ai funghi"};
+			
+			final ArrayList<String> lista_primi = new ArrayList<String>();
+			for (int i = 0; i < primi.length; ++i) {
+				lista_primi.add(primi[i]);
+			}
+			ListView listaprimi = (ListView) findViewById(R.id.list_view_primi);
+			
+			final MyArrayAdapter adapter_primi = new MyArrayAdapter(this,
+					layout.layout_list_view, lista_primi);
+			listaprimi.setAdapter(adapter_primi);
+			
+			//riempio la ListView per i secondi piatti
+			
+			String[] secondi = { "Scaloppine al limone",
+					"Ossobuco alla romana", "Stinco con Patate" };
+			
+			final ArrayList<String> lista_secondi = new ArrayList<String>();
+			for (int i = 0; i < secondi.length; ++i) {
+				lista_secondi.add(secondi[i]);
+			}
+			ListView listasecondi = (ListView) findViewById(R.id.list_view_secondi);
+			
+			final MyArrayAdapter adapter_secondi = new MyArrayAdapter(this,
+					layout.layout_list_view, lista_secondi);
+			listasecondi.setAdapter(adapter_secondi);
+			
+			//riempio la ListView per i contorni caldi
+			
+			String[] contorni = {"Fagioli", "Patatine fritte", "Carote" };
+			
+			final ArrayList<String> lista_contorni = new ArrayList<String>();
+			for (int i = 0; i < contorni.length; ++i) {
+				lista_contorni.add(contorni[i]);
+			}
+			ListView listacontorni = (ListView) findViewById(R.id.list_view_contorni);
+			
+			final MyArrayAdapter adapter_contorni = new MyArrayAdapter(this,
+					layout.layout_list_view, lista_contorni);
+			listacontorni.setAdapter(adapter_contorni);
 			
 		}
 	@Override
