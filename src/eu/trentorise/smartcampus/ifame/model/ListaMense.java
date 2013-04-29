4945fcd2ce6de11787eec1ef90c1b40042077d3b
francesco.maturi <francesco.maturi@3790a6ea-ec04-4a19-b1cd-c9a0372488fe>
2013-04-29 15:11:07 +0000

    ListaMense

---
+++ src/eu/trentorise/smartcampus/ifame/model/ListaMense.java
@@ -0,0 +1,17 @@
+package eu.trentorise.smartcampus.ifame.model;
+
+import java.util.List;
+
+public class ListaMense {
+
+	private List<Mensa> list;
+
+	public List<Mensa> getList() {
+		return list;
+	}
+
+	public void setList(List<Mensa> list) {
+		this.list = list;
+	}
+
+}
