7d488ffb150c98f12cdd58084e57ed8d1549c29b
desmond.agyeman <desmond.agyeman@3790a6ea-ec04-4a19-b1cd-c9a0372488fe>
2013-04-29 10:52:08 +0000

    Webcam images have been made larger

--- src/eu/trentorise/smartcampus/ifame/activity/IFretta_Details.java
+++ src/eu/trentorise/smartcampus/ifame/activity/IFretta_Details.java
@@ -28,10 +28,12 @@ public class IFretta_Details extends Activity {
 		Bundle extras = getIntent().getExtras();
 		setContentView(R.layout.ifretta_details);
 
+		//if there are no available intents  return
 		if (extras == null) {
 			return;
 		}
 
+		//Get mensa intent from activity:ifretta
 		Mensa m = (Mensa) extras.get("mensa");
 		TextView mensa_name = (TextView) findViewById(R.id.mensa_name_textview);
 		final TextView date = (TextView) findViewById(R.id.date_text_view);
@@ -39,6 +41,7 @@ public class IFretta_Details extends Activity {
 		SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
 		String date_s = s.format(new Date());
 
+		//the onclick method is for a demonstraive purpose, time is updated when the button is clicked
 		Button btn = (Button) findViewById(R.id.refresh_button);
 		btn.setOnClickListener(new OnClickListener() {
 			@Override
@@ -53,12 +56,17 @@ public class IFretta_Details extends Activity {
 		mensa_name.setText(String.valueOf(m));
 		date.setText(date_s);
 
-		String img_url = (String) extras.get("img_url");
+		String img_url = (String) extras.get("img_url"); //Get the mensa url  from activity:ifretta
 		
+		//if there is no webcam available for the given mensa, assign an image that says "not available"
 		if(img_url.equals("")){
 			findViewById(R.id.imageViewID).setBackgroundResource(R.drawable.image_not_available);
 		}	else {
-			new RetrieveImage((ImageView) findViewById(R.id.imageViewID)).execute(img_url); 
+			//retrieve the image from unitn website
+			ImageView img_view = (ImageView) findViewById(R.id.imageViewID);
+			img_view.getLayoutParams().height = 600; //Set size of the retrieved image 
+			img_view.getLayoutParams().width = 420; //set size of the retrieved image
+			new RetrieveImage(img_view).execute(img_url); 
 		}
 
 		// mensa_name.setTextColor(Color.parseColor("#228B22"));
@@ -66,27 +74,32 @@ public class IFretta_Details extends Activity {
 		//
 		// btn.setBackgroundColor(Color.parseColor("#228B22"));
 	}
-
+	
+	//It is advisable to use async task to retrieve data or perform network task
+	//Otherwise an exception will be thrown 
+	//This class will be called in the onCreate method
 	private class RetrieveImage extends AsyncTask<String, Void, Bitmap> {
 
 		ImageView img_view;
 
+		//Constructor..
 		public RetrieveImage(ImageView img_view) {
 			this.img_view = img_view;
 		}
 
+		//Perform retrieval in background
 		@Override
 		protected Bitmap doInBackground(String... urls) {
 			String urldisplay = urls[0];
-			Bitmap mIcon11 = null;
+			Bitmap bmap_image = null;
 			try {
 				InputStream in = new java.net.URL(urldisplay).openStream();
-				mIcon11 = BitmapFactory.decodeStream(in);
+				bmap_image = BitmapFactory.decodeStream(in);
 			} catch (Exception e) {
 				Log.e("Error", e.getMessage());
 				e.printStackTrace();
 			}
-			return mIcon11;
+			return bmap_image;
 		}
 
 		protected void onPostExecute(Bitmap result) {
