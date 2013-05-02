78a3a0269f5efee3809f7703edab8c1efed08f6a
francesco.maturi <francesco.maturi@3790a6ea-ec04-4a19-b1cd-c9a0372488fe>
2013-05-02 13:20:30 +0000

    working version with integration of ifretta connector

--- src/eu/trentorise/smartcampus/ifame/activity/IFretta.java
+++ src/eu/trentorise/smartcampus/ifame/activity/IFretta.java
@@ -4,10 +4,24 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import org.w3c.dom.Text;
+
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import eu.trentorise.smartcampus.ifame.R;
 import eu.trentorise.smartcampus.ifame.connector.IFrettaConnector;
 import eu.trentorise.smartcampus.ifame.model.ListaMense;
@@ -39,7 +53,7 @@ public class IFretta extends Activity {
 					getApplicationContext()).execute().get();
 
 			if (list != null) {
-				// createWebcamList(list);
+				createWebcamList(list);
 			}
 
 		} catch (InterruptedException e) {
@@ -50,116 +64,51 @@ public class IFretta extends Activity {
 			e.printStackTrace();
 		}
 	}
-	/*
-	 * private void createWebcamList(ListaMense list) { ListView
-	 * ifretta_listView = (ListView) findViewById(R.id.ifretta_page_list);
-	 * 
-	 * MyArrayAdapter adapter = new MyArrayAdapter(this,
-	 * R.layout.mensa_text,list); ifretta_listView.setAdapter(adapter);
-	 * 
-	 * 
-	 * 
-	 * } /* mensa_list = new ArrayList<WebcamMensa>();
-	 * 
-	 * ifretta_listView = (ListView) findViewById(R.id.ifretta_page_list);
-	 * 
-	 * MyArrayAdapter adapter = new MyArrayAdapter(this, R.layout.mensa_text,
-	 * mensa_list); ifretta_listView.setAdapter(adapter);
-	 * 
-	 * mensa_list.add(0, new WebcamMensa(Mensa.POVO_O, url_povo_0));
-	 * mensa_list.add(1, new WebcamMensa(Mensa.POVO_1, url_povo_1));
-	 * mensa_list.add(2, new WebcamMensa(Mensa.FBK, "")); mensa_list.add(3, new
-	 * WebcamMensa(Mensa.TOMMASO_GAR, url_tom_gar)); mensa_list.add(4, new
-	 * WebcamMensa(Mensa.XXIV_MAGGIO, "")); mensa_list.add(5, new
-	 * WebcamMensa(Mensa.ZANELLA, url_zanella)); mensa_list.add(6, new
-	 * WebcamMensa(Mensa.MESIANO, url_mesiano));
-	 * 
-	 * ifretta_listView.setOnItemClickListener(new OnItemClickListener() {
-	 * 
-	 * public void onItemClick(AdapterView<?> adapter, View v, int position,
-	 * long id) {
-	 * 
-	 * if (position == 0) { WebcamMensa val_at = mensa_list.get(0);
-	 * 
-	 * Intent i = new Intent(IFretta.this, IFretta_Details.class); //
-	 * i.setData(Uri.parse(url_povo_0)); //
-	 * i.setAction(Intent.ACTION_GET_CONTENT); //
-	 * i.addCategory(Intent.CATEGORY_OPENABLE); i.putExtra("mensa",
-	 * val_at.getTipo_mensa()); i.putExtra("img_url", val_at.getLink_webcam());
-	 * startActivity(i); }
-	 * 
-	 * if (position == 1) { WebcamMensa val_at = mensa_list.get(1);
-	 * 
-	 * Intent i = new Intent(IFretta.this, IFretta_Details.class);
-	 * i.putExtra("mensa", val_at.getTipo_mensa()); i.putExtra("img_url",
-	 * val_at.getLink_webcam()); startActivity(i);
-	 * 
-	 * }
-	 * 
-	 * if (position == 2) { WebcamMensa val_at = mensa_list.get(2);
-	 * 
-	 * Intent i = new Intent(IFretta.this, IFretta_Details.class);
-	 * i.putExtra("mensa", val_at.getTipo_mensa()); i.putExtra("img_url",
-	 * val_at.getLink_webcam()); startActivity(i);
-	 * 
-	 * }
-	 * 
-	 * if (position == 3) { WebcamMensa val_at = mensa_list.get(3);
-	 * 
-	 * Intent i = new Intent(IFretta.this, IFretta_Details.class);
-	 * i.putExtra("mensa", val_at.getTipo_mensa()); i.putExtra("img_url",
-	 * val_at.getLink_webcam()); startActivity(i);
-	 * 
-	 * }
-	 * 
-	 * if (position == 4) { WebcamMensa val_at = mensa_list.get(4);
-	 * 
-	 * Intent i = new Intent(IFretta.this, IFretta_Details.class);
-	 * i.putExtra("mensa", val_at.getTipo_mensa()); i.putExtra("img_url",
-	 * val_at.getLink_webcam()); startActivity(i);
-	 * 
-	 * }
-	 * 
-	 * if (position == 5) { WebcamMensa val_at = mensa_list.get(5); Intent i =
-	 * new Intent(IFretta.this, IFretta_Details.class); i.putExtra("mensa",
-	 * val_at.getTipo_mensa()); i.putExtra("img_url", val_at.getLink_webcam());
-	 * startActivity(i);
-	 * 
-	 * }
-	 * 
-	 * if (position == 6) { WebcamMensa val_at = mensa_list.get(6);
-	 * 
-	 * Intent i = new Intent(IFretta.this, IFretta_Details.class);
-	 * i.putExtra("mensa", val_at.getTipo_mensa()); i.putExtra("img_url",
-	 * val_at.getLink_webcam()); startActivity(i); } } });
-	 * 
-	 * }
-	 * 
-	 * class MyArrayAdapter extends ArrayAdapter<WebcamMensa> {
-	 * 
-	 * // HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
-	 * 
-	 * public MyArrayAdapter(Context context, int textViewResourceId,
-	 * List<WebcamMensa> objects) { super(context, textViewResourceId, objects);
-	 * }
-	 * 
-	 * @Override public View getView(int position, View convertView, ViewGroup
-	 * parent) {
-	 * 
-	 * LayoutInflater inflater = (LayoutInflater) getContext()
-	 * .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	 * 
-	 * convertView = inflater.inflate(R.layout.mensa_text, null);
-	 * 
-	 * TextView nome_mensa = (TextView) convertView
-	 * .findViewById(R.id.mensa_nameView); // Typeface myTypeface =
-	 * Typeface.createFromAsset(getAssets(), // "segoeuil.ttf"); //
-	 * nome_mensa.setTypeface(myTypeface); // TextView url_mensa = (TextView)
-	 * convertView // .findViewById(R.id.mensa_urlView);
-	 * 
-	 * WebcamMensa w_mensa = getItem(position);
-	 * nome_mensa.setText(w_mensa.getTipo_mensa() + " "); //
-	 * url_mensa.setText(w_mensa.getLink_webcam() + " "); return convertView; }
-	 */
 
+	private void createWebcamList(ListaMense list) {
+		ListView ifretta_listView = (ListView) findViewById(R.id.ifretta_page_list);
+
+		Adapter adapter = new WebArrayAdapter(this,
+				android.R.layout.simple_list_item_1, list.getList());
+
+		ifretta_listView.setAdapter((ListAdapter) adapter);
+
+		ifretta_listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> adapter, View view,
+					int position, long arg3) {
+				Mensa m = (Mensa) adapter.getItemAtPosition(position);
+				Intent i = new Intent(IFretta.this, IFretta_Details.class);
+				i.putExtra("mensa", m.getMensa_name());
+				i.putExtra("img_url", m.getMensa_link());
+				startActivity(i);
+			}
+		});
+	}
+}
+
+class WebArrayAdapter extends ArrayAdapter<Mensa> {
+
+	// HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+	Context context;
+	List<Mensa> mlist;
+
+	public WebArrayAdapter(Context context, int textViewResourceId,
+			List<Mensa> mlist) {
+		super(context, textViewResourceId, mlist);
+		this.context = context;
+		this.mlist = mlist;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		TextView tv = new TextView(context);
+		// setPadding(top, left, right, bottom)
+		tv.setPadding(10, 20, 10, 20);
+		tv.setTextSize(15);
+		tv.setText(mlist.get(position).getMensa_name());
+
+		return tv;
+	}
 }
