234b8d50de6ff2e2fe875f174a7365c7e53f1a87
stefano <stefano@stefano-K54HR>
2013-05-13 11:26:47 +0200

    Aggiunto dialog su IDECISO > menu del giorno - permette di lanciare la ricerca google del piatto selezionato

--- src/eu/trentorise/smartcampus/ifame/activity/Menu_giorno.java
+++ src/eu/trentorise/smartcampus/ifame/activity/Menu_giorno.java
@@ -5,12 +5,19 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.SearchManager;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -19,6 +26,8 @@ import eu.trentorise.smartcampus.ifame.R;
 import eu.trentorise.smartcampus.ifame.R.layout;
 
 public class Menu_giorno extends Activity {
+	
+	private String selectedDish;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -31,15 +40,15 @@ public class Menu_giorno extends Activity {
 		date.setText(daily_menu);
 
 		Button alternative_btn = (Button) findViewById(R.id.alternative);
-		alternative_btn.setOnClickListener(new OnClickListener () {
+		alternative_btn.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(Menu_giorno.this, Menu_giorno_alternative.class);
+				Intent intent = new Intent(Menu_giorno.this,
+						Menu_giorno_alternative.class);
 				startActivity(intent);
 			}
-			
-			
+
 		});
 
 		// riempio la ListView per i primi piatti
@@ -51,6 +60,19 @@ public class Menu_giorno extends Activity {
 			lista_primi.add(primi[i]);
 		}
 		ListView listaprimi = (ListView) findViewById(R.id.list_view_primi);
+		listaprimi.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View arg1,
+					int position, long arg3) {
+				selectedDish = (String) parent.getItemAtPosition(position);
+				StartWebSearchAlertDialog dialog = new StartWebSearchAlertDialog();
+				
+
+				dialog.show(getFragmentManager(), null);
+
+			}
+		});
 
 		final MyArrayAdapter adapter_primi = new MyArrayAdapter(this,
 				layout.layout_list_view, lista_primi);
@@ -83,6 +105,20 @@ public class Menu_giorno extends Activity {
 		final MyArrayAdapter adapter_contorni = new MyArrayAdapter(this,
 				layout.layout_list_view, lista_contorni);
 		listacontorni.setAdapter(adapter_contorni);
+		listasecondi.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View arg1,
+					int position, long arg3) {
+				selectedDish = (String) parent.getItemAtPosition(position);
+				StartWebSearchAlertDialog dialog = new StartWebSearchAlertDialog();
+				
+
+				dialog.show(getFragmentManager(), null);
+
+			}
+		});
+
 
 		// RISOLUZIONE PROBLEMA LISTVIEW IN SCROLLCONTAINER
 
@@ -116,6 +152,20 @@ public class Menu_giorno extends Activity {
 		paramsContorni.height = heightContorni;
 		listacontorni.setLayoutParams(paramsContorni);
 		listacontorni.requestLayout();
+		listacontorni.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View arg1,
+					int position, long arg3) {
+				selectedDish = (String) parent.getItemAtPosition(position);
+				StartWebSearchAlertDialog dialog = new StartWebSearchAlertDialog();
+				
+
+				dialog.show(getFragmentManager(), null);
+
+			}
+		});
+
 
 	}
 
@@ -124,6 +174,36 @@ public class Menu_giorno extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu_giorno, menu);
 		return true;
+	}
+
+	public class StartWebSearchAlertDialog extends DialogFragment {
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+			
+			
+			
+			builder.setMessage(getString(R.string.GoogleSearchAlertText) +" "+ selectedDish +"?")
+            .setPositiveButton(getString(R.string.GoogleSearchAlertAccept), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+    				
+                	Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+    				intent.putExtra(SearchManager.QUERY, selectedDish); // query contains
+    																// search string
+    				startActivity(intent);
+                    
+                }
+            })
+            .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // User cancelled the dialog
+                }
+            });
+     // Create the AlertDialog object and return it
+     return builder.create();
+
+			
+		}
 	}
 
 }
