1cfe8743e33779434dbabac21bd439c3318152c5
franz <franz@franz-AOD260>
2013-05-13 09:56:53 +0200

    Res added without bins

---
+++ src/eu/trentorise/smartcampus/ifame/model/Settimana.java
@@ -0,0 +1,20 @@
+package eu.trentorise.smartcampus.ifame.model;
+
+import java.util.Arrays;
+
+public class Settimana {
+
+		private Integer settimana_id;
+
+		public Settimana() {
+		}
+
+		public Integer getSettimana_id() {
+			return settimana_id;
+		}
+
+		public void setSettimana_id(Integer settimana_id) {
+			this.settimana_id = settimana_id;
+		}
+
+	}
