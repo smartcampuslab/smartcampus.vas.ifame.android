71c24b87c5b5186e0e48969724ebc0611774ea33
francescomaturi <francesco.maturi.paparot@gmail.com>
2013-05-22 15:44:26 +0200

    menu_mese connected fully with web service -> now gets the entire menu of the month and show the current week

--- src/eu/trentorise/smartcampus/ifame/activity/Menu_mese.java
+++ src/eu/trentorise/smartcampus/ifame/activity/Menu_mese.java
@@ -23,6 +23,7 @@ import eu.trentorise.smartcampus.android.common.Utils;
 import eu.trentorise.smartcampus.ifame.R;
 import eu.trentorise.smartcampus.ifame.R.layout;
 import eu.trentorise.smartcampus.ifame.model.MenuDelGiorno;
+import eu.trentorise.smartcampus.ifame.model.MenuDelMese;
 import eu.trentorise.smartcampus.ifame.model.MenuDellaSettimana;
 import eu.trentorise.smartcampus.ifame.model.PiattoKcal;
 import eu.trentorise.smartcampus.protocolcarrier.ProtocolCarrier;
@@ -38,6 +39,7 @@ public class Menu_mese extends Activity {
 	private Spinner weekSpinner;
 	private ListView listacibi_view;
 	ProgressDialog pd;
+	private MenuDelMese menuDelMese;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +51,8 @@ public class Menu_mese extends Activity {
 		pd = new ProgressDialog(Menu_mese.this).show(Menu_mese.this, "iDeciso",
 				"Loading...");
 
-		new IDecisoConnector(Menu_mese.this).execute();
-
+		// new MenuDellaSettimanaConnector(Menu_mese.this).execute();
+		new MenuDelMeseConnector(Menu_mese.this).execute();
 	}
 
 	@Override
@@ -60,7 +62,22 @@ public class Menu_mese extends Activity {
 		return true;
 	}
 
-	public class IDecisoConnector extends
+	/*
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * CONNECTOR MENU DELLA SETTIMANA
+	 */
+	public class MenuDellaSettimanaConnector extends
 			AsyncTask<Void, Void, MenuDellaSettimana> {
 
 		private ProtocolCarrier mProtocolCarrier;
@@ -68,7 +85,7 @@ public class Menu_mese extends Activity {
 		public String appToken = "test smartcampus";
 		public String authToken = "aee58a92-d42d-42e8-b55e-12e4289586fc";
 
-		public IDecisoConnector(Context applicationContext) {
+		public MenuDellaSettimanaConnector(Context applicationContext) {
 			context = applicationContext;
 		}
 
@@ -76,7 +93,7 @@ public class Menu_mese extends Activity {
 			mProtocolCarrier = new ProtocolCarrier(context, appToken);
 			MessageRequest request = new MessageRequest(
 					"http://smartcampuswebifame.app.smartcampuslab.it",
-					"getmenudelmese");
+					"getmenudellasettimana");
 			request.setMethod(Method.GET);
 			MessageResponse response;
 			try {
@@ -118,15 +135,16 @@ public class Menu_mese extends Activity {
 			ArrayAdapter<PiattoKcal> adapter = new ListHeaderAdapter(
 					Menu_mese.this, entireweek);
 
-			for (MenuDelGiorno m : mdglist) {
+			for (MenuDelGiorno mdg : mdglist) {
 				// ATTENZIONE AL MAGHEGGIO
+				PiattoKcal piattoSentinella = new PiattoKcal();
 				// setto come nome del piatto il numero del giorno
-				// del menu del giorno che sto iterando perche mi serve come
-				// sentinella nell'adapter
-				PiattoKcal p = new PiattoKcal();
-				p.setPiatto("" + m.getDay());
-				adapter.add(p);
-				adapter.addAll(m.getPiattiDelGiorno());
+				piattoSentinella.setPiatto(mdg.getDay() + "");
+				// del menu del giorno che sto iterando perche mi serve
+				// come sentinella nell'adapter
+				adapter.add(piattoSentinella);
+				// aggiungo tutti gli altri piatti
+				adapter.addAll(mdg.getPiattiDelGiorno());
 			}
 
 			listacibi_view.setAdapter(adapter);
@@ -183,4 +201,167 @@ public class Menu_mese extends Activity {
 			return convertView;
 		}
 	}
+
+	/*
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * CONNECTOR MENU DEL MESE
+	 */
+	public class MenuDelMeseConnector extends
+			AsyncTask<Void, Void, MenuDelMese> {
+
+		private ProtocolCarrier mProtocolCarrier;
+		public Context context;
+		public String appToken = "test smartcampus";
+		public String authToken = "aee58a92-d42d-42e8-b55e-12e4289586fc";
+
+		public MenuDelMeseConnector(Context applicationContext) {
+			context = applicationContext;
+		}
+
+		private MenuDelMese getMenuDelMese() {
+			mProtocolCarrier = new ProtocolCarrier(context, appToken);
+			MessageRequest request = new MessageRequest(
+					"http://smartcampuswebifame.app.smartcampuslab.it",
+					"getmenudelmese");
+			request.setMethod(Method.GET);
+			MessageResponse response;
+			try {
+				response = mProtocolCarrier.invokeSync(request, appToken,
+						authToken);
+				if (response.getHttpStatus() == 200) {
+					String body = response.getBody();
+					MenuDelMese mdm = Utils.convertJSONToObject(body,
+							MenuDelMese.class);
+					return mdm;
+				}
+			} catch (ConnectionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		protected MenuDelMese doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			return getMenuDelMese();
+		}
+
+		@Override
+		protected void onPostExecute(MenuDelMese mdm) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(mdm);
+			// setto il menu del mese ricevuto come variabile di classe
+			menuDelMese = mdm;
+			// cerco la settimana corrente e la mostro
+			Calendar c = Calendar.getInstance();
+			int currentDay = c.get(Calendar.DAY_OF_MONTH);
+			// prendo la lista di menu della settimana
+			ArrayList<MenuDellaSettimana> mds = (ArrayList<MenuDellaSettimana>) mdm
+					.getMenuDellaSettimana();
+			// creo la lista di piatti da mostrare
+			ArrayList<PiattoKcal> currentWeek = new ArrayList<PiattoKcal>();
+			// creo l'adapter per la lista di piatti
+			ArrayAdapter<PiattoKcal> adapter = new ListHeaderAdapter(
+					Menu_mese.this, currentWeek);
+			// ciclo sulle settimane e prendo tutti i piatti della settimana
+			// corrente
+			for (MenuDellaSettimana m : mds) {
+				int start_day = m.getStart_day();
+				int end_day = m.getEnd_day();
+				// se il giorno corrente è tra il giorno iniziale e quelo finale
+				// della settimana sono nella settimana che mi interessa
+				if (currentDay >= start_day && currentDay <= end_day) {
+					// sono nella settimana interessata ciclo sui menu del
+					// giorno
+					ArrayList<MenuDelGiorno> mdglist = (ArrayList<MenuDelGiorno>) m
+							.getMenuDelGiorno();
+					for (MenuDelGiorno mdg : mdglist) {
+						// ATTENZIONE AL MAGHEGGIO
+						PiattoKcal piattoSentinella = new PiattoKcal();
+						// setto come nome del piatto il numero del giorno
+						piattoSentinella.setPiatto(mdg.getDay() + "");
+						// del menu del giorno che sto iterando perche mi serve
+						// come sentinella nell'adapter
+						adapter.add(piattoSentinella);
+						// aggiungo tutti gli altri piatti
+						adapter.addAll(mdg.getPiattiDelGiorno());
+					}
+				}
+			}
+
+			listacibi_view.setAdapter(adapter);
+			// chiudo il loading...
+			pd.dismiss();
+		}
+	}
+
+	// private class ListHeaderAdapter extends ArrayAdapter<PiattoKcal> {
+	//
+	// public ListHeaderAdapter(Context context, List<PiattoKcal> list) {
+	// super(Menu_mese.this, android.R.layout.simple_list_item_1, list);
+	// }
+	//
+	// @Override
+	// public View getView(int position, View convertView, ViewGroup parent) {
+	//
+	// LayoutInflater inflater = (LayoutInflater) getContext()
+	// .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	//
+	// convertView = inflater
+	// .inflate(layout.layout_row_menu_adapter, null);
+	//
+	// PiattoKcal p = getItem(position);
+	//
+	// TextView name = (TextView) convertView
+	// .findViewById(R.id.menu_name_adapter);
+	// TextView kcal = (TextView) convertView
+	// .findViewById(R.id.menu_kcal_adapter);
+	//
+	// if (p.getPiatto().matches("[0-9]+")) {
+	// // ho un piatto sentinella setto il testo come data
+	// // e niente kcal
+	// int day = Integer.parseInt(p.getPiatto());
+	//
+	// Calendar c = Calendar.getInstance();
+	// c.set(Calendar.DATE, day);
+	// SimpleDateFormat s = new SimpleDateFormat("EEEE dd MMMM yyyy");
+	// String date_formatted = s.format(c.getTime());
+	//
+	// convertView.setBackgroundColor(Color.RED);
+	// name.setText(date_formatted);
+	// name.setTextSize((float) 20);
+	// // name.setBackgroundColor(Color.RED);
+	// name.setTextColor(Color.WHITE);
+	// // perche nel layout c'è che esce 'kcal'
+	// kcal.setText("");
+	//
+	// } else {
+	// // ho un piatto vero setto i campi coi valori corrispondenti
+	// name.setText(p.getPiatto());
+	// kcal.setText(p.getKcal());
+	// }
+	// return convertView;
+	// }
+	// }
 }
