f89e20152d1d93264c3d50defb13f75ec48ba996
desmond-agyeman <joekobby@gmail.com>
2013-05-29 16:25:23 +0200

    "Alternative al menu del giorno" is now taken directly from the excel file

---
+++ src/eu/trentorise/smartcampus/ifame/activity/IFretta_Details.java
@@ -0,0 +1,89 @@
+package eu.trentorise.smartcampus.ifame.activity;
+
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import eu.trentorise.smartcampus.ifame.R;
+
+public class IFretta_Details extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Bundle extras = getIntent().getExtras();
+		setContentView(R.layout.ifretta_details);
+
+		//if there are no available intents  return
+		if (extras == null) {
+			return;
+		}
+
+		//Get mensa intent from activity:ifretta
+	    String mensa_name = (String) extras.get("mensa");
+		setTitle(mensa_name);
+		SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+		String date_s = s.format(new Date());
+
+		String img_url = (String) extras.get("img_url"); //Get the mensa url  from activity:ifretta
+		
+		//if there is no webcam available for the given mensa, assign an image that says "not available"
+		if(img_url.equals("")){
+			findViewById(R.id.imageViewID).setBackgroundResource(R.drawable.image_not_available);
+		}	else {
+			//retrieve the image from unitn website
+			ImageView img_view = (ImageView) findViewById(R.id.imageViewID);
+			img_view.getLayoutParams().height = 600; //Set size of the retrieved image 
+			img_view.getLayoutParams().width = 420; //set size of the retrieved image
+			new RetrieveImage(img_view).execute(img_url); 
+		}
+
+		// mensa_name.setTextColor(Color.parseColor("#228B22"));
+		// date.setTextColor(Color.parseColor("#228B22"));
+		//
+		// btn.setBackgroundColor(Color.parseColor("#228B22"));
+	}
+	
+	//It is advisable to use async task to retrieve data or perform network task
+	//Otherwise an exception will be thrown 
+	//This class will be called in the onCreate method
+	private class RetrieveImage extends AsyncTask<String, Void, Bitmap> {
+
+		ImageView img_view;
+
+		//Constructor..
+		public RetrieveImage(ImageView img_view) {
+			this.img_view = img_view;
+		}
+
+		//Perform retrieval in background
+		@Override
+		protected Bitmap doInBackground(String... urls) {
+			String urldisplay = urls[0];
+			Bitmap bmap_image = null;
+			try {
+				InputStream in = new java.net.URL(urldisplay).openStream();
+				bmap_image = BitmapFactory.decodeStream(in);
+			} catch (Exception e) {
+				Log.e("Error", e.getMessage());
+				e.printStackTrace();
+			}
+			return bmap_image;
+		}
+
+		protected void onPostExecute(Bitmap result) {
+			img_view.setImageBitmap(result);
+		}
+
+	}
+}
