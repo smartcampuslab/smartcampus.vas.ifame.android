faaa67a8a2af6b423a41456060517c0af57ca05b
desmond-agyeman <joekobby@gmail.com>
2013-07-04 11:52:38 +0200

    testing isoldi encoding

--- src/eu/trentorise/smartcampus/ifame/activity/ISoldi.java
+++ src/eu/trentorise/smartcampus/ifame/activity/ISoldi.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.concurrent.ExecutionException;
 
+import org.apache.http.entity.StringEntity;
+
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
@@ -145,15 +147,10 @@ public class ISoldi extends SherlockActivity {
 	}
 
 	public void getAmount(float amount) {
-		String euro_string = "€ ";
+
 		if (amount >= 4.90) {
-			// set utf-8 enconding on the euro string
-			try {
-				euro_string.getBytes("UTF8");
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
-			}
-			centerText.setText(euro_string + String.valueOf(amount));
+	
+			centerText.setText( String.valueOf(amount));
 			centerText.setTextColor(Color.parseColor("#228B22"));
 
 			bottomText.setText(getString(R.string.iSoldi_puoi_acquistare));
@@ -168,7 +165,7 @@ public class ISoldi extends SherlockActivity {
 			adapter.notifyDataSetChanged();
 
 		} else if (amount >= 4.20 && amount < 4.90) {
-			centerText.setText(euro_string + String.valueOf(amount));
+			centerText.setText(String.valueOf(amount));
 			centerText.setTextColor(Color.parseColor("#FFD700"));
 
 			bottomText.setText(getString(R.string.iSoldi_puoi_acquistare));
@@ -181,7 +178,7 @@ public class ISoldi extends SherlockActivity {
 			adapter.notifyDataSetChanged();
 
 		} else if (amount >= 2.90 && amount < 4.20) {
-			centerText.setText(euro_string + String.valueOf(amount));
+			centerText.setText(String.valueOf(amount));
 			centerText.setTextColor(Color.parseColor("#FF8800"));
 
 			bottomText.setText(getString(R.string.iSoldi_puoi_acquistare));
@@ -191,7 +188,7 @@ public class ISoldi extends SherlockActivity {
 					.add(getString(R.string.iDeciso_menu_types_snack));
 			adapter.notifyDataSetChanged();
 		} else {
-			centerText.setText(euro_string + String.valueOf(amount));
+			centerText.setText(String.valueOf(amount));
 			centerText.setTextColor(Color.parseColor("#CC0000"));
 
 			bottomText.setText(getString(R.string.iSoldi_devi_ricaricare));
