a00c0d2349b2222915bf3f085c85699d9e69e890
francescomaturi <francesco_maturi@tiscali.it>
2013-10-18 18:37:30 +0200

    cleaning and cleaning

---
+++ src/eu/trentorise/smartcampus/ifame/model/AspectRatioImageView.java
@@ -0,0 +1,34 @@
+package eu.trentorise.smartcampus.ifame.model;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class AspectRatioImageView extends ImageView {
+
+	public AspectRatioImageView(Context context) {
+		super(context);
+	}
+
+	public AspectRatioImageView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public AspectRatioImageView(Context context, AttributeSet attrs,
+			int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int width = widthMeasureSpec;
+		int height = heightMeasureSpec;
+		if (getDrawable() != null) {
+			width = MeasureSpec.getSize(widthMeasureSpec);
+			height = width * getDrawable().getIntrinsicHeight()
+					/ getDrawable().getIntrinsicWidth();
+
+		}
+		setMeasuredDimension(width, height);
+	}
+}
